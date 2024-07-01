a8ebfec51ce3c464e329668c7dd0e362b24e7f2c
PARNO <aplikasi.pinppspm@gmail.com>
2024-07-01 02:58:03 +0000

    ok

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,32 @@
-#import library needed
-import pandas as pd
-import streamlit as st 
-import pickle
 import numpy as np
+import pandas as pd
+import streamlit as st
+import pickle
 
-# Load the best model
-with open('iris_model.pkl', 'rb') as f:
+# Load the model 
+with open('house_xgb_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-species = ['setosa', 'versicolor', 'virginica']
+st.title("California Housing Prediction App")
+st.sidebar.header("Input Features")
+housing_median_age = st.sidebar.slider('Average age of house (years)', 0.0, 200.0, 50.0)
+median_income = st.sidebar.number_input('Median income (in millions)', 0.00, 20.00, 3.87)
+population = st.sidebar.slider('Population in the vicinity (millions)', 50.00, 20000.00, 1500.00)
+households = st.sidebar.slider('Number of households', 0, 7000, 500)
+total_rooms = st.sidebar.slider('Total room area', 0, 50000, 2000)
+total_bedrooms = st.sidebar.slider('Total bedroom area', 0, 50000, 500)
+lat = st.sidebar.number_input("Latitude", 32.5121, 42.0126, 37.3, step=0.1)
+long = st.sidebar.number_input("Longitude", -124.6509, -114.1315, -122.37, step=0.1)
 
-st.title("Iris Flower Classification")
-st.write("This app correctly classifies iris flower among 3 possible species")
+if st.button('Estimate Price'):
+    # Prepare input features (assuming your model expects these features in this order)
+    input_features = np.array([housing_median_age, total_rooms, total_bedrooms, population, 
+                               households, median_income, lat, long]).reshape(1, -1)
 
-# Creating Sidebar for inputs
-st.sidebar.title("Inputs")
-sepal_length = st.sidebar.number_input("sepal length (cm)", 4.3, 7.9, 5.0)
-sepal_width = st.sidebar.number_input("sepal width (cm)", 2.0, 4.4, 3.6)
-petal_length = st.sidebar.number_input("petal length (cm)", 1.0, 6.9, 1.4)
-petal_width = st.sidebar.number_input("petal width (cm)", 0.1, 2.5, 0.2)
+    # Make prediction
+    with st.spinner("Calculating..."):
+        predicted_price = model.predict(input_features)  # Use model.predict 
 
-# Button to trigger prediction
-if st.button("Predict"):
-# Getting Prediction from model
-    inp = np.array([sepal_length, sepal_width, petal_length, petal_width])
-    inp = np.expand_dims(inp, axis=0)
-    prediction = model.predict(inp)
+    st.success(f"Estimated House Price: ${predicted_price[0]:,.2f}")
 
-# Show Results when the button is clicked
-    result = species[np.argmax(prediction)]
-    st.write("**This flower belongs to " + result + " class**")
 
